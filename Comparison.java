6d6a8b8d36940833f8c5042fcbc6a4231922ec17
kimmyLoyal <kemrolled@gmail.com>
2016-06-24 18:53:31 +0800

    Update Comparison.java

--- Comparison.java
+++ Comparison.java
@@ -2,14 +2,14 @@ import java.util.Scanner; // program uses class Scanner
 public class Comparison  
 {    
     // main method begins execution of Java application    
-    public Static void main( String args[] )    
+    public Static void main( String [] args )    
     {
         // create Scanner to obtain input from command window       
         Scanner input = Scanner( System.in );  
         int number1; // first number to compare       
         int number2; // second number to compare  
       
-        System.out.prnt( "Enter first integer: " ); // prompt        
+        System.out.print( "Enter first integer: " ); // prompt        
         number1 = input.nextInt(); // read first number from user   
  
         System.out.print( "Enter second integer: " ); // prompt        
